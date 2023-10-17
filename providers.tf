b6d37cfd162efa655fb83eeb06566745b746b91d
ritam-mishra <ritammishra2002@gmail.com>
2023-10-17 16:11:11 +0000

    #21 change the structure of root module and add tagging to the bucket.

---
+++ providers.tf
@@ -0,0 +1,25 @@
+terraform {
+  cloud {
+    organization = "terraform-beginner-project"
+
+    workspaces {
+      name = "Terrahouse"
+    }
+  }
+  required_providers {
+    random = {
+      source = "hashicorp/random"
+      version = "3.5.1"
+    }
+    aws = {
+      source = "hashicorp/aws"
+      version = "5.16.2"
+    }
+  }
+}
+
+provider "aws" {
+}
+provider "random" {
+  # Configuration options
+}
